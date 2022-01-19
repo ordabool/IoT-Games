2305112c3d96ed55a1518548b2281b1e604d73fb
Or Dabool <or.dabool@gmail.com>
2022-01-19 22:22:42 +0200

    Ran project 2 on Elegoo

--- Arduino/project2/project2.ino
+++ Arduino/project2/project2.ino
@@ -8,8 +8,10 @@ const int SWITCH = 2;
 const int GREEN_LED = 3;
 const int RED_LED_1 = 4;
 const int RED_LED_2 = 5;
-const int CYCLE_TIME = 250;
+const int CYCLE_TIME = 150;
 bool steadyLight;
+bool isLight1Active = false;
+bool isLight2Active = false;
 
 void setup() {
   pinMode(GREEN_LED, OUTPUT);
@@ -27,20 +29,28 @@ void loop() {
   if (switchState == HIGH) {
     Serial.println("Pressed!");
     steadyLight = !steadyLight;
-    delay(500);
+    delay(250);
   }
   
   if (steadyLight) {
     digitalWrite(GREEN_LED, HIGH);
     digitalWrite(RED_LED_1, LOW);
     digitalWrite(RED_LED_2, LOW);
+    isLight1Active = false;
+    isLight2Active = false;
   } else {
     digitalWrite(GREEN_LED, LOW);
-    digitalWrite(RED_LED_1, LOW);
-    digitalWrite(RED_LED_2, HIGH);
-    delay(CYCLE_TIME);
-    digitalWrite(RED_LED_1, HIGH);
-    digitalWrite(RED_LED_2, LOW);
+    if (!isLight1Active) {
+      digitalWrite(RED_LED_1, HIGH);
+      digitalWrite(RED_LED_2, LOW);
+      isLight1Active = true;
+      isLight2Active = false;
+    } else {
+      digitalWrite(RED_LED_1, LOW);
+      digitalWrite(RED_LED_2, HIGH);
+      isLight1Active = false;
+      isLight2Active = false;
+    }
     delay(CYCLE_TIME);
   }
 }
